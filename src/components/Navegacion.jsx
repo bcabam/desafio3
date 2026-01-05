259c0b57b021411d2e84322c4b24212af6b43e1d
Benjamin Caba <bca4ba@gmail.com>
2026-01-04 23:45:35 -0300

    más ajustes tediosos

--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -3,14 +3,13 @@ import Logo from '../assets/emojione--shortcake.png'
 
 export const Navegacion = () => {
     return (
-        <nav className="menu"> {}
-            {}
+        <nav className="menu">
             <div className='links'>
                 <Link to='/' style={{marginRight: '20px', textDecoration:'none', color:'white'}}>Home</Link>
                 <Link to='/contacto' style={{textDecoration:'none', color:'white'}}>Contacto</Link>
             </div>
             
-            {}
+            
             <div className='brand'>
                 <span style={{color:'white', marginRight:'10px', fontWeight:'bold'}}>Happy Cake</span>
                 <img src={Logo} alt="Logo Cake" width={50}/>
